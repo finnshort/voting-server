1aad73aa169b70d4a2494d8d24bb9ec0abc26e30
finnshort <finn.d.short@gmail.com>
2017-06-04 16:51:51 -0700

    complete app logic

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,12 +1,13 @@
 import {expect} from 'chai';
 import {List, Map} from 'immutable';
 
+//Numbers are naturally immutable- ie would say 42 + 1 = 43
+//not 42.setState(43);
 
 describe('immutability', () => {
-
   describe('a number', () => {
 
-    function increment(currentState) {
+    function increment(currentState){
       return currentState + 1;
     }
 
@@ -17,12 +18,13 @@ describe('immutability', () => {
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
     });
-
   });
 
-  describe('A List', () => {
 
-    function addMovie(currentState, movie) {
+//Can apply the same logic to a data structure like a list
+  describe('a list', () => {
+
+    function addMovie(currentState, movie){
       return currentState.push(movie);
     }
 
@@ -40,17 +42,30 @@ describe('immutability', () => {
         '28 Days Later'
       ));
     });
-
   });
 
-  describe('a tree', () => {
 
+//Could also apply to a map structure with key 'movies' that contains a list of movies
+//adding a movie creates a new map, and movie key points to a new list
+
+  describe('a tree', () => {
+    /*
+    // Old function
     function addMovie(currentState, movie) {
       return currentState.set(
         'movies',
         currentState.get('movies').push(movie)
       );
     }
+    */
+
+
+    //to operate on nested data structures, immutable provides helper function to reach into data structure
+    //for ex 'update':
+
+    function addMovie(currentState, movie) {
+        return currentState.update('movies', movies => movies.push(movie));
+      }
 
     it('is immutable', () => {
       let state = Map({
