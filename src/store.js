4c0f07836899ca956211194080fc9c907bd2a29d
finnshort <finn.d.short@gmail.com>
2017-06-05 17:28:04 -0700

    reducer composition

---
+++ src/store.js
@@ -0,0 +1,15 @@
+/* If we had a collection of every action that would ever occur in our app, we
+// could just call reduce. but in reality actions come in at all different times,
+// and we need something to 'store' the state of the application over time.
+*/
+
+/* the store is initialized with the reducer function and then we dispatch
+// actions to the store:
+import {createStore} from 'redux';
+const store = createStore(reducer);
+// The store will internally use your reducer to apply actions to the current
+// state, and save the next state.
+store.dispatch({type: 'NEXT'});
+// You can get the current state any time:
+store.getState();
+*/
---
+++ test/store_spec.js
@@ -0,0 +1,6 @@
+import { Map, fromJS } from 'immutable';
+import { expect } from 'chai';
+
+import makeStore from '../src/store';
+
+describe 
